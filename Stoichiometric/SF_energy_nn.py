a45cc3996ed4f5f20c773adf5216237214a10a7f
Lu Wenyu <121702968+wylu-github@users.noreply.github.com>
2026-05-25 01:01:47 +0800

    Update SF_energy_nn.py

--- Stoichiometric/SF_energy_nn.py
+++ Stoichiometric/SF_energy_nn.py
@@ -1,19 +1,5 @@
 import torch.nn as nn
 
-
-# class SFEnergyNN(nn.Module):
-#     def __init__(self, input_size, hidden_size, output_size):
-#         super(SFEnergyNN, self).__init__()
-#         self.fc1 = nn.Linear(input_size, hidden_size)
-#         self.fc2 = nn.Linear(hidden_size, output_size)
-#         self.relu = nn.ReLU()
-#
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = self.fc2(x)
-#         return x
-
-
 class SFEnergyNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(SFEnergyNN, self).__init__()
